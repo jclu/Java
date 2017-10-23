ee1741f114ecbce5683b34758eea9b16a18e7be1
jclu <jclu@users.noreply.github.com>
2017-10-23 11:59:55 -0500

    Update Mockup.java

--- Mockup.java
+++ Mockup.java
@@ -1,3 +1,5 @@
+1.
+-------------
         MockUp<Properties> propertiesMockUp = new MockUp<Properties>() {
             @Mock
             public String getProperty(Invocation inv, String value) {
@@ -11,3 +13,46 @@
                 }
             }
         };
+
+2. (https://stackoverflow.com/questions/23162777/how-do-i-mock-an-autowired-value-field-in-spring-with-mockito)
+--------------
+Given:
+
+@Value("#{myProps[‘default.url']}")
+private String defaultUrl;
+In test method (qualified name just for completeness here):
+
+org.springframework.test.util.ReflectionTestUtils.setField(
+        myClassUnderTest, "defaultUrl", "url123");
+// Note: Don't use MyClassUnderTest.class
+// Note: Don't use the referenced string "#{myProps[‘default.url']}", 
+//       but simply the field name "defaultUrl"
+
+
+https://stackoverflow.com/questions/17353327/populating-spring-value-during-unit-test
+    
+3.
+--------------
+Spring allows you to provide an instance of PropertySourcesPlaceholderConfigurer which will be used to populate the values of the fields annotated with @Value().
+
+/**
+ * Creates the mock properties that will be used for this test. 
+ * These will be used mainly to populate the {@code @Value("")} fields.
+ * 
+ * @return the mock properties source place holders configure
+ */
+@Bean
+public static PropertySourcesPlaceholderConfigurer properties() {
+  final PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
+  /* 
+   * Instead of loading the properties from the file system, the test makes use of the following 
+   * hard-coded properties 
+   * pspc.setLocation(new ClassPathResource("project.properties"));
+   */
+  final Properties properties = new Properties();
+  properties.setProperty("property.value1", "value1");
+  properties.setProperty("property.value2", "value2");
+  properties.setProperty("property.valueN", "valueN");
+  pspc.setProperties(properties);
+  return pspc;
+}
